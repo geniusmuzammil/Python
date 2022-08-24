0f4431908e024f106587f7e2ba1ff6168a6f1cc1
Muzammil <88898994+geniusmuzammil@users.noreply.github.com>
2022-08-24 23:06:18 +0500

    Update main.py

--- Intermidiate/US-states-game/main.py
+++ Intermidiate/US-states-game/main.py
@@ -1,8 +1,9 @@
 import turtle
 import pandas
+
+
 screen = turtle.Screen()
 screen.title("US States")
-
 screen.addshape("./blank_states_img.gif")
 turtle.shape("./blank_states_img.gif")
 data = pandas.read_csv("./50_states.csv")
@@ -18,8 +19,8 @@ while len(guessed_states) <= 50:
             if state not in guessed_states:
                 states_to_learn.append(state)
         pandas.DataFrame(states_to_learn).to_csv("States_to_learn.csv")
-
         break
+        
     if guess in all_states_data:
         guessed_states.append(guess)
         place_holder = turtle.Turtle()
@@ -28,7 +29,4 @@ while len(guessed_states) <= 50:
         state_data = data[data["state"] == guess]
         place_holder.goto(int(state_data.x), int(state_data.y))
         place_holder.write(arg= f"{guess}")
-
-
-
 
